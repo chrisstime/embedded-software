d406dd3389ea69941039aa175f640f9b9769e378
chrisstime <christine.vinaviles@gmail.com>
2017-08-10 11:11:57 +1000

    Changes to packet files

--- Sources/packet.h
+++ Sources/packet.h
@@ -44,4 +44,23 @@ bool Packet_Get(void);
  */
 bool Packet_Put(const uint8_t command, const uint8_t parameter1, const uint8_t parameter2, const uint8_t parameter3);
 
+/*! @brief Shift all the packets by one by passing their value to the previous packet.
+ *
+ *  @void  - this is purely a function.
+ */
+void Packet_Shift();
+
+/*! @brief Calculates the checksum by XORing the values of Packet Command to Packet 3.
+ *
+ *  @return uint8_t - returns the calculated checksum value
+ */
+uint8_t Calculated_Checksum(void);
+
+/*! @brief Compares the actual value of checksum with the calculated checksum based on the Checksum_Check() function.
+ *  static because we will only ever use this within this .c file
+ *
+ *  @return bool - TRUE if calculated checksum matches the value checksum packet.
+ */
+static bool Checksum_Check(void);
+
 #endif
